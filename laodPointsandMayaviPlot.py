7ed05d7e5336eac9d6ae8333d6e4e57da9ace71b
Kota Miura <miura@embl.de>
2011-12-02 19:35:08 +0100

    point loading and plot in Mayavi, modified

--- laodPointsandMayaviPlot.py
+++ laodPointsandMayaviPlot.py
@@ -2,6 +2,9 @@
 #20111202
 #Kota Miura (miura@embl.de)
 
+filename = 'c:/dropbox/My Dropbox/Mette/Tracks.csv'
+#filename = '/Users/miura/Dropbox/Mette/Tracks.csv'
+
 #import csv
 # data = csv.reader(open('C:/dropbox/My Dropbox/Pairs_NowCorrectDot.txt', 'rb'), delimiter='\t')
 #data = csv.reader(open('/Users/miura/Dropbox/Mette/Tracks.csv'))
@@ -17,38 +20,58 @@
 
 from matplotlib import mlab as matp
 
-filename = 'c:/dropbox/My Dropbox/Mette/Tracks.csv'
-#filename = '/Users/miura/Dropbox/Mette/Tracks.csv'
-ind, trajID, tA, x1, y1, z1, ptID = matp.load(filename, skiprows=1, delimiter=',', usecols=[0, 1, 2, 3, 4, 5, 6], unpack=True)
+ind, trajID, tA, x1, y1, z1, sx, sy, sz, ptID = matp.load(filename, skiprows=1, delimiter=',', usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], unpack=True)
 
 #for d in ind:
     #print d
 
 from mayavi.mlab import points3d
 from mayavi.mlab import plot3d
+from mayavi.mlab import quiver3d
+from mayavi.mlab import flow
 from mayavi import mlab as maya
 
-p1s = points3d(x1, y1, z1, scale_factor=.75, color=(1, 0, 0))
+p1s = points3d(sx, sy, sz, scale_factor=.45, color=(1, 0, 0))
 
 curtid = -1.0
 cx = []
 cy = []
 cz = []
 #for row, tid in trajID:
+#for d in ind:
+    #if (curtid - trajID[d-1]) != 0:
+        #print 'start trajec', trajID[d-1]
+        #if len(cx) > 1:
+          #print 'plotting', curtid, 'length:', len(cx)
+          #plot3d(cx, cy, cz, line_width=14.0, color=(0, 1, 1))
+        #curtid = trajID[d-1]
+        #cx = []
+        #cy = []
+        #cz = []
+    #cx.append(sx[d-1])
+    #cy.append(sy[d-1])
+    #cz.append(sz[d-1])
+
+vx = []
+vy = []
+vz = []
+px = []
+py = []
+pz = []
+curid = -1.0
 for d in ind:
-    if (curtid - trajID[d-1]) != 0:
-        print 'start trajec', trajID[d-1]
-        if len(cx) > 1:
-          print 'plotting', curtid, 'length:', len(cx)
-          plot3d(cx, cy, cz, color=(0, 1, 1))
+    if (curtid - trajID[d-1]) == 0:
+        #print 'in trajec', trajID[d-1]
+        px.append(sx[d-2])
+        py.append(sy[d-2])
+        pz.append(sz[d-2])
+        vx.append(sx[d-1] - sx[d-2])
+        vy.append(sy[d-1] - sy[d-2])
+        vz.append(sz[d-1] - sz[d-2])
+    else:
         curtid = trajID[d-1]
-        cx = []
-        cy = []
-        cz = []
-    cx.append(x1[d-1])
-    cy.append(y1[d-1])
-    cz.append(z1[d-1])
-
-
+        print 'trajectory: ', trajID[d-1]
+quiver3d(px, py, pz, vx, vy, vz, color=(0, 1, 1), opacity=0.3, mode='2darrow', scale_factor=1)
+#flow(px, py, pz, vx, vy, vz, color=(0, 1, 1), opacity=0.7 )
 maya.show()
 
